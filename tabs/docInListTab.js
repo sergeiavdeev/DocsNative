1ef00e7664591f9637d784bbfacf665da8b3d2fe
avdeev.sa <avdeev.sa@pecom.ru>
2020-03-11 17:16:29 +0300

    Init

---
+++ tabs/docInListTab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+class DocInListTab extends React.Component{
+
+  render() {
+    return <View>
+      <Text>Входящие</Text>
+    </View>;
+  }
+} export default DocInListTab
---
+++ tabs/taskListTab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+class TaskListTab extends React.Component{
+
+  render() {
+    return <View>
+      <Text>Задачи</Text>
+    </View>;
+  }
+} export default TaskListTab
